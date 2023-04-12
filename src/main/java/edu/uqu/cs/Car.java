b3ed7695d7fabd9091315517f499941ebf25b656
SumiahSaed <127794930+SumiahSaed@users.noreply.github.com>
2023-04-12 14:17:37 +0000

    Commiting from Codespace

--- src/main/java/edu/uqu/cs/Car.java
+++ src/main/java/edu/uqu/cs/Car.java
@@ -63,6 +63,37 @@ public class Car{
      * moveCarIn() to set the inOutGarage flag to true;
      *
      */
+     private String model;
+    private boolean inOutGarage;
+
+     public Car(){
+     model="";
+     inOutGarage=false;
+    }
+
+     public String getModel() {
+        return this.model;
+    }
+    public boolean getInOutGarage() {
+        return this.inOutGarage;
+    }
+
+     public void setModel(String model) {
+        this.model = model;
+    }
+    public void setInOutGarage(boolean inOutGarage) {
+        this.inOutGarage = inOutGarage;
+    }
+
+     public void moveCarOut(){
+       this.inOutGarage=false; 
+       
+    }
+    
+    public void moveCarIn(){
+        this.inOutGarage=true;
+    }
+
 
 
 
